8e2a02c96820dd685956158b663f4e05a5b1c169
翁治平 <gocreating@gmail.com>
2018-06-25 19:55:31 +0800

    Add hint of ownerId handler

---
+++ server/models/project.js
@@ -0,0 +1,13 @@
+module.exports = function(Project) {
+  // Project.observe('before save', function(ctx, next) {
+  //   var accessToken = ctx.options && ctx.options.accessToken;
+  //   var userId = accessToken && accessToken.userId;
+  //
+  //   if (ctx.instance) {
+  //     ctx.instance.ownerId = userId;
+  //   } else {
+  //     ctx.data.ownerId = userId;
+  //   }
+  //   return next();
+  // });
+}
