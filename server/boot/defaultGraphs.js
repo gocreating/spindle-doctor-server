86fadc0e94b6d74c80440f26fafee1ab064c70fc
翁治平 <gocreating@gmail.com>
2018-06-28 01:51:25 +0800

    Add Graph model

---
+++ server/boot/defaultGraphs.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var async = require('async');
+
+module.exports = function defaultAdmins(app) {
+  var Graph = app.models.Graph;
+  var graphInfo = require('../graph.json');
+
+  async.eachSeries(graphInfo.graphs, (graph, callback) => {
+    Graph.findOrCreate(
+      { where: { name: graph.name } },
+      graph,
+      function(err, graph) {
+        if (err) throw err;
+        console.log('Graph', graph.name, 'created');
+        callback();
+      }
+    );
+  }, (err) => {
+    if (err) throw err;
+  });
+};
